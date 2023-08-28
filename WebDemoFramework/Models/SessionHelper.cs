35bf0e0f3daee15cc6ab3104026fc019d3017835
rovingwind <rovingwind93@gmail.com>
2023-08-28 20:19:42 +0800

    update

--- WebDemoFramework/Models/SessionHelper.cs
+++ WebDemoFramework/Models/SessionHelper.cs
@@ -3,6 +3,6 @@ using SA = LibraryFramework.Services.SessionAccessor;
 
 namespace WebDemoFramework.Models {
   public static class SessionHelper {
-    public static int? Count { get { return SA.Get<int>(); } set { value.Set(); } }
+    public static int? Count { get { return SA.Get<int>(); } set { SA.Set(value); } }
   }
 }
