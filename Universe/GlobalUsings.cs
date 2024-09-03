3b8fe7eb9d29d082e690b8c888db55a6245e20c5
Leo Shen <rovingwind93@gmail.com>
2024-09-03 18:21:24 +0800

    update

--- Universe/GlobalUsings.cs
+++ Universe/GlobalUsings.cs
@@ -1,5 +1,7 @@
-﻿global using Microsoft.AspNetCore.Http;
+﻿global using CoreAudio;
+global using Microsoft.AspNetCore.Http;
 global using System;
+global using System.Diagnostics;
 global using System.Runtime.CompilerServices;
 global using System.Runtime.InteropServices;
 global using System.Text.Json;
---
+++ Universe/Services/SV_Process.cs
@@ -0,0 +1,24 @@
+﻿namespace Universe.Services;
+
+public class SV_Process {
+  public static void ExecuteCommand(string command) {
+    ProcessStartInfo processStartInfo = new("cmd", "/c " + command) {
+      RedirectStandardOutput = true,
+      UseShellExecute = false,
+      CreateNoWindow = true
+    };
+
+    using Process process = new() { StartInfo = processStartInfo };
+    ExcuteProcess(process);
+  }
+
+  public static void ExecuteProcessStartInfo(ProcessStartInfo processStartInfo) {
+    using Process process = new() { StartInfo = processStartInfo };
+    ExcuteProcess(process);
+  }
+
+  public static void ExcuteProcess(Process process) {
+    process.Start();
+    process.WaitForExit();
+  }
+}
